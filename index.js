3c09bd4588997899f95fb3ddeba631cadfe6f164
Isai Subuyuj <isubuyuj-2021346@kinal.edu.gt>
2023-01-18 14:52:49 -0600

    Commit Uno

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function sumar(){
+    const valor1 = document.getElementById('valor1').value;
+    const valor2 = document.getElementById('valor2').value;
+
+    let suma = parseInt(numero1) + parseInt(numero2);
+    console.log(suma);
+}
